d0a2ae1461238cc904134c4d6f70f40ddd22b86d
sohail0 <sohailman5202@gmail.com>
2024-10-26 14:24:53 +0530

    Selenium test is ready

---
+++ src/main/java/ai1/sohail.java
@@ -0,0 +1,18 @@
+package ai1;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class sohail {
+    public static void main(String[] args) {
+        // Set the path to the ChromeDriver executable
+       
+        // Initialize WebDriver instance
+        WebDriver driver = new ChromeDriver();
+        
+        // Open a website
+        driver.navigete().to("https://javatpoint.com");
+        
+        // Close the browser
+        
+    }
+}
